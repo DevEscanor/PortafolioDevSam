33333da26f44e2ad9907d50da7c4135914d13bc2
David Samuel Suárez Montoya <devsamsuarez@gmail.com>
2025-08-03 01:48:18 -0600

    🐛 Fix: Remove unused useEffect import to fix build error

--- src/hooks/useScrollSnap.js
+++ src/hooks/useScrollSnap.js
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-
 export const useScrollSnap = () => {
   // Scroll completamente normal - sin funcionalidad especial
   // El scroll funcionará como en cualquier sitio web normal
